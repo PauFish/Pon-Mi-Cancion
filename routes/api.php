f406f1a2be89ddef18034cbe64393a2b2860162b
Pau Siquier <pausiqrey8@gmail.com>
2022-11-25 19:35:28 +0100

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PartyController;
+use App\Http\Controllers\SongController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+ //   return $request->user();
+//});
+
+Route::resource('roles',RoleController::class);
+
+Route::resource('users',UserController::class);
+Route::resource('parties',PartyController::class);
+Route::resource('songs',SongController::class);
