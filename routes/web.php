7aad671ba1c6736755e1ff8ab6490d3d1221cd8e
Pau Siquier <pausiqrey8@gmail.com>
2022-12-03 18:41:14 +0100

    Comments has to be fixed 2

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,9 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\SongController;
 use App\Http\Controllers\HomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+ /*Rutas*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -34,32 +27,26 @@ Route::resource('roles', RoleController::class);
 
 Auth::routes();
   
-/*------------------------------------------
-All Normal Users Routes List
---------------------------------------------*/
+/*Usuarios*/
 Route::middleware(['auth', 'user-access:user'])->group(function () {
   
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 });
   
-/*------------------------------------------
-All Admin Routes List
---------------------------------------------*/
+/*Admin*/
 Route::middleware(['auth', 'user-access:admin'])->group(function () {
   
     Route::get('adminHome', [HomeController::class, 'adminHome'])->name('admin.home');
     
 });
   
-/*------------------------------------------
-All Admin Routes List
---------------------------------------------*/
+/*Dj*/
 Route::middleware(['auth', 'user-access:manager'])->group(function () {
   
     Route::get('managerHome', [HomeController::class, 'managerHome'])->name('manager.home');
 });
  
-
+/*rutas de enlace necesarias*/
 Route::get('/djSong', function () {
     return view('djSong');
 });
